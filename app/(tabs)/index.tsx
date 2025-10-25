2466b7b71461a4ac31455e75e5e5821ed059b3b9
Dan Wu <d.wu@sleekflow.io>
2025-10-25 17:24:19 +0800

    wip

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
-import { Unauthenticated } from "convex/react";
-import { Redirect } from "expo-router";
+import { Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function Index() {
   return (
-    <Unauthenticated>
-      <Redirect href="/sign-in" />
-    </Unauthenticated>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Herrooo</Text>
+    </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,43 +1,12 @@
-import { useColorScheme } from "@/hooks/use-color-scheme";
-import { tokenCache } from "@/utils/cache";
-import { ClerkLoaded, ClerkProvider, useAuth } from "@clerk/clerk-expo";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { ConvexReactClient } from "convex/react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { Slot } from "expo-router";
-import "react-native-reanimated";
-import { SafeAreaView } from "react-native-safe-area-context";
-import "../global.css";
-
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
-  unsavedChangesWarning: false,
-});
-
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <SafeAreaView>
-      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-        <ClerkLoaded>
-          <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-            <ThemeProvider
-              value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-            >
-              <Slot />
-            </ThemeProvider>
-          </ConvexProviderWithClerk>
-        </ClerkLoaded>
-      </ClerkProvider>
-    </SafeAreaView>
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{ headerShown: false, title: "Home" }}
+      />
+    </Stack>
   );
 }
